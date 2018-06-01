6e05fd35002982330dcfa3dc5fb526aa75e509df
Jboy Flaga <flaga.jeremiah@gmail.com>
2018-06-01 10:58:19 +0800

    - refactoring - using array.join() instead of string concatenation

--- 2018-06-01-javascript-james-sinclair/flickr-fetcher.js
+++ 2018-06-01-javascript-james-sinclair/flickr-fetcher.js
@@ -3,8 +3,12 @@ var FlickrFetcher;
 
 FlickrFetcher = {
     photoObjToURL: function(photoObj) {
-        return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' +
-            photoObj.secret + '_b.jpg';
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/', 
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
     }
 };
 
